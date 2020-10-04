d2269bbd6e3a6b9f230f6154ad2919562809128e
Alexander Mischuk <mischuk.alexander@gmail.com>
2020-10-04 18:31:04 +0300

    Updates

---
+++ precache-manifest.c55a7a810b467ebec642b1fe183a36f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34be418b4f7fc429d34a1b9c3176ace9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "355ededd3ec7a7d83860",
+    "url": "./static/css/2.fd29c4ed.chunk.css"
+  },
+  {
+    "revision": "f99b9d06d958e0cb1cfe",
+    "url": "./static/css/main.76987d30.chunk.css"
+  },
+  {
+    "revision": "355ededd3ec7a7d83860",
+    "url": "./static/js/2.f6c413cf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.f6c413cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f99b9d06d958e0cb1cfe",
+    "url": "./static/js/main.6aac868a.chunk.js"
+  },
+  {
+    "revision": "deca9bc2d6b467a730e7",
+    "url": "./static/js/runtime-main.d9711a4d.js"
+  }
+]);
