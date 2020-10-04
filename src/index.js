7c989c3cccb5e18bb72f1bd3b6dff34d717133a0
Alexander Mischuk <mischuk.alexander@gmail.com>
2020-10-04 19:04:42 +0300

    fix: mobile layout styles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import Game from "./components/Game";
 import { store } from "./store";
 import "../node_modules/ress/ress.css";
+import "./index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
